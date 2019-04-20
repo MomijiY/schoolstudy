fd7be5c44614dd0137174ba52ad8c863536a5fc3
吉川椛 <momiji@yoshikawanomacbook-pro.local>
2019-04-20 16:49:38 +0900

    Initial Commit

---
+++ schoolstudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  schoolstudy
+//
+//  Created by 吉川椛 on 2019/04/20.
+//  Copyright © 2019 吉川椛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
